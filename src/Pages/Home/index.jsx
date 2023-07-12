4cfa29d6160d814cda7b52fd38a9912686876fa1
SimonGarbet <simon.garbet@gmail.com>
2023-07-12 17:47:53 +0200

    Bouton Gravité v1

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,14 +1,86 @@
 import React from 'react'
+import { useEffect } from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBath, faGolfBallTee, faPoo } from '@fortawesome/free-solid-svg-icons'
+
+import '../../Scss/home.scss'
 
 
 function Home() {
 
+useEffect(() => {
+
+
+ document.querySelectorAll('.gravityButton').forEach(btn => {
+
+ btn.addEventListener('mousemove', (e) => {
+    
+    const rect = btn.getBoundingClientRect();    
+    const h = rect.width / 2;
+    
+    const x = e.clientX - rect.left - h;
+    const y = e.clientY - rect.top - h;
+
+    const r1 = Math.sqrt(x*x+y*y);
+    const r2 = (1 - (r1 / h)) * r1;
+
+    const angle = Math.atan2(y, x);
+    const tx = Math.round(Math.cos(angle) * r2 * 100) / 100;
+    const ty = Math.round(Math.sin(angle) * r2 * 100) / 100;
+    
+    const op = (r2 / r1) + 0.25;
+
+    btn.style.setProperty('--tx', `${tx}px`);
+    btn.style.setProperty('--ty', `${ty}px`);
+    btn.style.setProperty('--opacity', `${op}`);
+  });
+
+  btn.addEventListener('mouseleave', (e) => {
+    btn.style.setProperty('--tx', '0px');
+    btn.style.setProperty('--ty', '0px');
+    btn.style.setProperty('--opacity', `${0.25}`);
+  });
+
+
+});
+
+});
+
+
+
+
   return (
     <section >
         
-      <h1>Voici ma page d'accueil</h1>
+      <h1></h1>
+      
 
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates ad veniam praesentium harum, eveniet necessitatibus ratione impedit assumenda, hic beatae nam numquam omnis non sequi eum delectus voluptate qui quod.</p>
+    
+      <div className='buttonLine'>
+
+      <div className = "gravityButton">
+  <button >
+  <FontAwesomeIcon icon={faBath} style={{color: "#b81919",}} />
+  </button>
+    </div>
+
+<div className = "gravityButton">
+  <button>
+  <FontAwesomeIcon icon={faPoo} className='buttonIcon' style={{color: "#19BE2C",}}/>
+  </button>
+</div>
+
+<div className = "gravityButton">
+  <button>
+  <FontAwesomeIcon icon={faGolfBallTee} className='buttonIcon' style={{color: "#198CBE", width: '50%', fill: 'var(--_fill, #111)', transition: 'fill 0.3s'}} />
+  </button>
+</div>
+
+</div>
+
+
     </section>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './Pages/Home/';
-import Error from './Pages/Error/'
+import Error from './Pages/Error/';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
