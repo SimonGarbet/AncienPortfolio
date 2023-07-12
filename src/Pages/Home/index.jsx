8ec140368f33865f5f0d0bfc6e035a3d6b04800c
SimonGarbet <simon.garbet@gmail.com>
2023-07-12 15:55:06 +0200

    Mise en place de l'environnement de base

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function Home() {
+
+  return (
+    <section >
+        
+      <h1>Voici ma page d'accueil</h1>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates ad veniam praesentium harum, eveniet necessitatibus ratione impedit assumenda, hic beatae nam numquam omnis non sequi eum delectus voluptate qui quod.</p>
+    </section>
+  )
+}
+
+export default Home
