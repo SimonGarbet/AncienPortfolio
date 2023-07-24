ce714b84d752470b53b76e5e641173126be612cc
SimonGarbet <simon.garbet@gmail.com>
2023-07-24 12:03:05 +0200

    Adpatation Bouton Gravité avec les outils React

--- src/Components/GravityButton/index.jsx
+++ src/Components/GravityButton/index.jsx
@@ -7,60 +7,59 @@ import '../../Scss/style.scss';
 
 
 
+
+
 function GravityButton({logo, size, color}){
+
+
   
+  const [translation, setTranslation] = useState({ tx: 0, ty: 0 });
 
-    useEffect(() => {
+  const handleMouseMove = (e) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    const h = rect.width / 2;
+
+    const x = e.clientX - rect.left - h;
+    const y = e.clientY - rect.top - h;
+
+    const r1 = Math.sqrt(x * x + y * y);
+    const r2 = (1 - r1 / h) * r1;
+
+    const angle = Math.atan2(y, x);
+    const tx = Math.round(Math.cos(angle) * r2 * 100) / 100;
+    const ty = Math.round(Math.sin(angle) * r2 * 100) / 100;
+
+    setTranslation({ tx, ty });
+  };
+
+  const handleMouseLeave = () => {
+    setTranslation({ tx: 0, ty: 0 });
+  };
 
 
-        document.querySelectorAll('.gravityButton').forEach(btn => {
-       
-        btn.addEventListener('mousemove', (e) => {
-           
-           const rect = btn.getBoundingClientRect();    
-           const h = rect.width / 2;
-           
-           const x = e.clientX - rect.left - h;
-           const y = e.clientY - rect.top - h;
-       
-           const r1 = Math.sqrt(x*x+y*y);
-           const r2 = (1 - (r1 / h)) * r1;
-       
-           const angle = Math.atan2(y, x);
-           const tx = Math.round(Math.cos(angle) * r2 * 100) / 100;
-           const ty = Math.round(Math.sin(angle) * r2 * 100) / 100;
-           
-           const op = (r2 / r1) + 0.25;
-       
-           btn.style.setProperty('--tx', `${tx}px`);
-           btn.style.setProperty('--ty', `${ty}px`);
-           btn.style.setProperty('--opacity', `${op}`);
-         });
-       
-         btn.addEventListener('mouseleave', (e) => {
-           btn.style.setProperty('--tx', '0px');
-           btn.style.setProperty('--ty', '0px');
-           btn.style.setProperty('--opacity', `${0.25}`);
-         });
-       
-       
-       });
-       
-       }, []);
 
-       const [animation, setAnimation] = useState(false);
 
-    useEffect(() => {
+  const [animation, setAnimation] = useState(false);
 
-        if (animation === true) {
-        setTimeout(() => {setAnimation(false)}, "2000" )
-        }
+  useEffect(() => {
+
+    if (animation === true) {
+    setTimeout(() => {setAnimation(false)}, "2000" )
+    }
         
-      }, [animation]);
+  }, [animation]);
 
 
     return (
-      <div onClick={() => setAnimation(true) } className = {animation ? 'gravityCopied' : 'gravityButton'}>
+      <div onClick={() => setAnimation(true) } 
+      className = {animation ? 'gravityCopied' : 'gravityButton'} 
+      onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseLeave}
+        style={{
+          '--tx': `${translation.tx}px`,
+          '--ty': `${translation.ty}px`,
+        }}
+      >
         <button className='buttonIconCheck'>
           <FontAwesomeIcon icon={faCircleCheck} size={size} style={{color: color,}} />
           </button>
